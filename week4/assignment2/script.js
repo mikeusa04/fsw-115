d94819ca40b459804e9788c34cdb9389af7272ad
mikeusa04 <71799572+mikeusa04@users.noreply.github.com>
2021-02-14 17:14:10 -0800

    week4 assignment

--- week4/assignment2/script.js
+++ week4/assignment2/script.js
@@ -1,3 +1,8 @@
+/*This website needs to have at least 2 pages, where each page presents a button
+When the button is clicked, an axios.get request will go to an API of the students choosing and have the response data show up as a list on the HTML page.
+Must be fully styled - responsive
+2 APIs must be used, (one for each page)*/
+
 
 const getMikeData = () => {
 axios.get("https://api.vschool.io/mikesaleh1/todo")
